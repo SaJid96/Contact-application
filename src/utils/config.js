24b5c0221e4009045aaeb2111e33d3f3a1be5940
Sajid96 <sajidhussain98@gmail.com>
2021-05-31 14:14:38 +0530

    first commit

---
+++ src/utils/config.js
@@ -0,0 +1,18 @@
+//TODO: add firebase configuration and export it
+export const firebaseConfig = {
+  apiKey: "AIzaSyAnutjRrjVSbMP-VIuNVEA9bPgx5_0qywE",
+  authDomain: "web-app-12ba1.firebaseapp.com",
+  databaseURL: "https://web-app-12ba1-default-rtdb.firebaseio.com",
+  projectId: "web-app-12ba1",
+  storageBucket: "web-app-12ba1.appspot.com",
+  messagingSenderId: "1044184542184",
+  appId: "1:1044184542184:web:d02f8095a52762bbded25d",
+  measurementId: "G-CVS0L4G45X"
+};
+//image configuration
+export const imageConfig = {
+  quality: 0.2,
+  maxWidth: 800,
+  maxHeight: 600,
+  autoRotate: true
+};
